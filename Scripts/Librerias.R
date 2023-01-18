7e06c2362a50a49d02f8361848571683965916d9
Andres Bedoya <andres.f-7@hotmail.com>
2023-01-17 23:03:59 -0500

    Librerias

---
+++ Scripts/Librerias.R
@@ -0,0 +1,25 @@
+
+
+#Función 
+install_and_load_packages <- function(packages) {
+# Revisar si paquetes están instalados
+not_installed <- packages[!(packages %in% installed.packages()[,"Package"])]
+# Instalar paquetes que no están instalados
+if (length(not_installed) > 0){
+  install.packages(not_installed, dependencies = TRUE)
+}
+# Cargar paquetes
+for (package in packages) {
+  library(package, character.only = TRUE)
+}
+}
+
+#Paquetes necesarios
+packages <- c("dplyr", "ggplot2", "lubridate", "tidyr", "kableExtra", "flextable", "filesstrings",
+              "purrr", "readr", "extrafont", "stringr", "ggmap", "readxl", "leaflet", "mapview", "purrr")
+
+install_and_load_packages(packages)
+rm(packages, install_and_load_packages)
+loadfonts(device = "win")
+
+#
