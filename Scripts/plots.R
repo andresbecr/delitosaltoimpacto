c2de07ba500d1912a00c1775c03a3c7f988f5efd
Andres Bedoya <andres.f-7@hotmail.com>
2023-02-02 21:30:45 -0500

    Prueba 2

--- Scripts/plots.R
+++ Scripts/plots.R
@@ -4,8 +4,11 @@ library(ggplot2)
 #Graficos y tablas
 #Df COUNT
 #Graficos
-#Día con mas hechos
-df_count <- df_2022 %>% 
+#Día con más hechos
+
+df <- df_2022
+
+df_count <- df %>% 
   group_by(nombre_dia) %>% 
   summarize(cuenta = n())
 
@@ -15,6 +18,52 @@ hechos_dias = ggplot(data=df_count, mapping=aes(x=nombre_dia, y= cuenta, fill=no
   labs(x = "Día", y = "Cantidad de hechos") + 
   scale_fill_manual(values = ifelse(df_count$nombre_dia == df_count$nombre_dia[which.max(df_count$cuenta)], "red", "white"), guide=guide_legend(title="Día con más hechos")) +
   theme_fivethirtyeight()
+
+hechos_dias
+
+
+#Delito con más hechos
+df_count <- df %>% 
+  group_by(delito) %>% 
+  summarize(cuenta = n()) 
+%>% 
+  arrange(desc(cuenta))
+
+df_count = head(df_count, n=10)
+
+hechos_delitos = ggplot(data=df_count, mapping=aes(y=delito, x= cuenta, fill=delito)) + 
+  geom_bar(stat="identity") + 
+  labs(y = "Delito", x = "Cantidad de hechos") + 
+  scale_fill_manual(values = ifelse(df_count$delito == df_count$delito[which.max(df_count$cuenta)], "blue", "white"), guide=guide_legend(title="Delito con más hechos")) +
+  theme_fivethirtyeight()
+
+hechos_delitos
+
+
+
+df_sum <- df %>% 
+  filter(localidad != "99 - SIN LOCALIZACION") %>%
+  group_by(localidad) %>% 
+  summarize(suma = sum(numero_hechos))
+
+#%>% arrange(desc(suma))
+
+df_sum$suma <- as.numeric(df_sum$suma)
+
+
+
+hechos_localidad = ggplot(data=df_sum, mapping=aes(y=localidad, x=suma, fill=localidad)) + 
+  geom_bar(stat="identity") + 
+  labs(y = "Localidad", x = "Cantidad de hechos") +
+  scale_fill_manual(values = ifelse(df_sum$localidad == df_sum$localidad[which.max(df_sum$suma)], "blue", "white"), guide=guide_legend(title="Delito con más hechos")) +
+  theme_fivethirtyeight()
+
+hechos_localidad
+
+
+
+
+heatmap(table$2022)
 
 # #Cuenta delitos
 # delitos=ggplot(data=df_2022, aes(y=delito)) + geom_bar(stat="count") + theme(aspect.ratio = 2) 
